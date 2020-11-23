dcb9efdd9d5599cfc2f2bf96b8291264d1de8aaa
Isaev <matthew.riesdale@gmail.com>
2020-11-24 02:19:56 +0300

    add sns subscription

--- product-service/src/catalogBatchProcess.js
+++ product-service/src/catalogBatchProcess.js
@@ -1,20 +1,28 @@
+const AWS = require('aws-sdk');
 const createProduct = require('../db/createProduct');
+const { REGION } = require('./values');
 
 module.exports = async event => {
+  const sns = new AWS.SNS({ region: REGION });
 
-  event.Records.forEach(async record => {
+  for (const record of event.Records) {
     try {
       const product = JSON.parse(record.body);
 
       const { title, artist, description, price: stringPrice, coverurl, count: stringCount } = product;
       const price = parseInt(stringPrice);
       const count = parseInt(stringCount);
-      console.log({title, artist, description, price, coverurl, count})
-      await createProduct(title, artist, description, price, coverurl, count);
+
+      await createProduct(title, artist, description, price, coverurl, count)
+
       console.log(`product was added: ${JSON.stringify(product)}`);
+      await sns.publish({
+        Subject: 'Vinyl store update',
+        Message: `New products were added: ${product.title} by ${product.artist}`,
+        TopicArn: process.env.SNS_TOPIC
+      }).promise();
     } catch (e) {
       console.error(`error during product insersion: ${e.message}`);
     }
-  });
+  };
 };
- 
